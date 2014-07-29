8fceff531fabfe1f56bd50a2dcd25561af58a746
dabit3 <dabit3@gmail.com>
2014-07-29 10:49:40 -0500

    Added if statements and associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718192040) do
+ActiveRecord::Schema.define(version: 20140728213531) do
 
   create_table "installs", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -35,7 +35,11 @@ ActiveRecord::Schema.define(version: 20140718192040) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
+    t.string   "user_address"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -50,6 +54,7 @@ ActiveRecord::Schema.define(version: 20140718192040) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "address"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
